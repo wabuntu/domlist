0de095d02dee038e6bbff74bc995928cf817aeec
ntobo <to.wabuntu@proton.me>
2023-08-14 16:59:32 +0900

    adding local mode

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,24 @@ use ssh2::Session;
 use std::env;
 use std::io::Read;
 use std::net::TcpStream;
+use std::process::Command;
 #[macro_use]
 extern crate prettytable;
 use prettytable::format;
 use prettytable::Table;
 use prettytable::{color, Attr};
 
+/// Specify host or run in local
 #[derive(Parser, Debug)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+)]
 struct Args {
-    #[arg(value_name = "host", help = "Target compute node name, or IP address")]
-    pos_arg: String,
+    /// Target host name in FQDN or IP address
+    host: Option<String>,
 }
 
 /// Status information set for each virsh domain
@@ -60,22 +68,42 @@ fn run_ssh(user: &str, host: &str, cmd: &str) -> String {
     return result;
 }
 
+// Run command in local
+fn run_command(cmd: &str) -> String {
+
+    let output = Command::new(cmd)
+                                    .output()
+                                    .expect("failed to execute process");
+    // println!("status: {}", output.status);
+    // println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    // println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    assert!(output.status.success());
+
+    return String::from_utf8_lossy(&output.stdout).to_string();
+}
+
 fn main() {
     const GIGA: i64 = 1000000000;
     const MEGA: i64 = 1000000;
     let mut vmstats_list: Vec<VMStats> = vec![];
+    let mut remote_mode: bool = true;
 
     // Get target node, port, user
     let args = Args::parse();
-    let host: String = format!("{}:22", args.pos_arg);
-    let user: String = env::var("USER").expect("$USER is not set");
+    remote_mode = if args.host != None {true} else {false};
+    let host: String = args.host.clone().unwrap_or(String::from("")) + ":22";
+    let user: String = if remote_mode {env::var("USER").expect("$USER is not set")} else {String::from("")};
+    println!(r"Connecting... : {}@{}", user, host);
 
     // Run 'virsh domstats' in target node
-    let domstats = run_ssh( &user,
-                            &host,
-                            "sudo virsh domstats --cpu-total --balloon --interface --block \
-                            | grep -e Domain: -e cpu.time -e balloon -e bytes -e allocation -e capacity");
-
+    let domstats: String = if remote_mode {
+                            run_ssh( &user, &host,
+                                "sudo virsh domstats --cpu-total --balloon --interface --block \
+                                | grep -e Domain: -e cpu.time -e balloon -e bytes -e allocation -e capacity")
+                        } else {
+                            run_command("sudo virsh domstats --cpu-total --balloon --interface --block \
+                                | grep -e Domain: -e cpu.time -e balloon -e bytes -e allocation -e capacity")
+                        };
     let mut index = 0;
     let mut cpu_top: i64 = 0;
     let mut io_top: i64 = 0;
@@ -148,7 +176,11 @@ fn main() {
         "| grep nova:name | sed -r 's/<nova:name>(.*)<\\/nova:name>/\\1/';",
         "done;"
     );
-    let instances = run_ssh(user.as_str(), host.as_str(), &cmd);
+    let instances = if remote_mode {
+                        run_ssh(user.as_str(), host.as_str(), &cmd)
+                    } else {
+                        run_command(&cmd)
+                    };
     let mut index = 0;
     for instance in instances.lines() {
         let instance = instance.trim();
